8e11debdc700d44080d7894b9e722dc91aa6d927
Jabesinhoo <jabesrko11@gmail.com>
2025-09-23 09:21:32 -0500

    actualizacion de middlewre de inicio

--- middleware/rateLimit.js
+++ middleware/rateLimit.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 // Limitar intentos de login
 const loginLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutos
+  windowMs: 1 * 60 * 1000, // 15 minutos
   max: 5, // máx 5 intentos por IP
   message: { error: 'Demasiados intentos de inicio de sesión. Inténtalo más tarde.' },
   standardHeaders: true, // devuelve info en cabeceras RateLimit-*
